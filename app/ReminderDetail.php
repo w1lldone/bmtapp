ab9ef4bb3bfc718e395d408c29613d0c2bab6fce
w1lldone <widan.arrahman@gmail.com>
2017-07-29 19:22:39 +0700

    add ReminderDetail model and table

---
+++ app/ReminderDetail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReminderDetail extends Model
+{
+    /*
+	* Relation section
+    */
+    public function reminder()
+    {
+        return $this->belongsTo('App\Reminder');
+    }
+
+    public function nasabah()
+    {
+        return $this->belongsTo('App\Nasabah');
+    }
+}
