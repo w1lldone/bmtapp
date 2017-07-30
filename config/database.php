89d07d76d2d19bc066b42df6dd78fa8cd4be459c
w1lldone <widan.arrahman@gmail.com>
2017-07-30 09:40:01 +0700

    update database config

--- config/database.php
+++ config/database.php
@@ -113,7 +113,7 @@ return [
             'driver' => 'mysql',
             'host' => 'localhost',
             'port' => '3306',
-            'database' => 'bu',
+            'database' => 'bmtbu',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
