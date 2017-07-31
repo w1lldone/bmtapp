2d9c3e017dad145611fa4c3948a6450cece52988
w1lldone <wildan.arrahman@gmail.com>
2017-07-31 14:24:37 +0700

    fix query for kretrans

--- app/Jobs/KreditReminder.php
+++ app/Jobs/KreditReminder.php
@@ -35,7 +35,7 @@ class KreditReminder implements ShouldQueue
     public function handle()
     {
         // get all nasabah from kretrans
-        $kredits = KretransBU::where('TGL_TRANS', $this->reminder->tanggal->toDateString())->get();
+        $kredits = KretransBU::where('TGL_TRANS', $this->reminder->tanggal->toDateString())->where('MY_KODE_TRANS', 200)->get();
 
         foreach ($kredits as $kredit) {
             // check registered nasabah
