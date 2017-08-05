322c58edb91414286f5d4f05aa4fae43f3371df5
w1lldone <wildan.arrahman@gmail.com>
2017-08-05 08:42:45 +0700

    add agenda model and migration

---
+++ database/migrations/2017_08_05_082610_create_agendas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('mku_id')->nullable()->unsigned();
+            $table->string('name', 250);
+            $table->string('tanggal', 12);
+            $table->string('deskripsi', 500)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendas');
+    }
+}
