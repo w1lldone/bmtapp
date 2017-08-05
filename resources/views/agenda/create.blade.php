8517a74455099f564646ed25b482a72348f41028
w1lldone <wildan.arrahman@gmail.com>
2017-08-05 21:18:02 +0700

    center the form{

--- resources/views/agenda/create.blade.php
+++ resources/views/agenda/create.blade.php
@@ -9,7 +9,7 @@
 
 @section('content')
 	<div class="row">
-		<div class="col-lg-6">
+		<div class="col-lg-6 col-lg-offset-3">
 			@include('layouts.status')
 			@include('layouts.errors')
 			<div class="card">
