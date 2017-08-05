aa503b9038ffb6cf686229a209b06587ea1483d0
w1lldone <wildan.arrahman@gmail.com>
2017-08-05 20:51:22 +0700

    add relation

--- app/Agenda.php
+++ app/Agenda.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Agenda extends Model
 {
+	/*
+	* Relation section
+	*/
+
+	public function mku(){
+		return $this->belongsTo('App\Mku');
+	}
+	
     protected $dates = ['tanggal'];
     protected $guarded = ['id'];
 }
