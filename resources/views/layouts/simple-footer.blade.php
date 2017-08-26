ff8ef4690045765ea57f1fc6dd1ceae9d3ce5b26
w1lldone <wildan.arrahman@gmail.com>
2017-08-26 10:50:32 +0700

    add simple footer view~

---
+++ resources/views/layouts/simple-footer.blade.php
@@ -0,0 +1,49 @@
+<!-- FOOTER -->
+<footer class="footer mt-5">
+  <div class="container py-4">
+    <div class="row">
+      <div class="col-12 text-center">
+        <img src="../images/logos/ugm.png" class="px-2 my-2" height="70px"> <br>
+        <hr style="border-color: #e3e3f3; width: 80%;" class="mx-auto my-4">
+      </div>
+    </div>
+    <div class="row pt-2">
+      <div class="col-md-3">
+        <h5><b>Hubungi kami</b></h5>
+        <p style="line-height: 2;">
+        <i class="fa fa-phone fa-fw" aria-hidden="true"></i>&nbsp;(0274) 798868<br>
+        <i class="fa fa-twitter fa-fw" aria-hidden="true"></i>&nbsp;<a href="https://twitter.com/bmtbinaummah">@bmtbinaummah</a><br>
+        <i class="fa fa-facebook-square fa-fw" aria-hidden="true"></i>&nbsp;<a href="https://www.facebook.com/binaummah">Bina Ummah</a><br>
+        <i class="fa fa-instagram fa-fw" aria-hidden="true"></i>&nbsp;<a href="http://instagram.com/binaummah">@binaummah</a>
+        </p>
+      </div>
+      <div class="col-md-3">
+        <h5><b>BMT Bina Ummah</b></h5>
+        <p style="line-height: 2;">
+          <a href="http://binaummah.org/tentang/">Tentang kami</a> <br>
+          <a href="http://binaummah.org/layanan/">Layanan</a> <br>
+          <a href="http://binaummah.org/semua-danasi/">Donasi</a>
+        </p>
+      </div>
+      <div class="col-md-3">
+        <h5><b>BMT Mobile Apps</b></h5>
+        <p style="line-height: 2;">
+          <a href="/aturan-penggunaan">Aturan penggunaan</a> <br>
+          <a href="/kebijakan-privasi">Kebijakan privasi</a>
+        </p>
+      </div>
+      <div class="col-md-3">
+        <p class="text-center"> <img src="/uploads/images/logo/logo.svg" height="70px"></p>
+        <p class="text-center">BMT Mobile App</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="container">
+        <hr class="mx-auto my-3" style="border-color: #e3e3f3; width: 80%;">
+        <p class="text-muted">
+          BMT Mobile App &copy; <a href="http://binaummah.org">BMT Bina Ummah</a> dikembangkan oleh <a href="http://patpatstudio.com">Patpat</a>. Logo BMT Mobile App &copy; <a href="https://www.facebook.com/romdon.well.allways">Mujahid Ramadan</a>. Website &copy; 2017 <a href="https://www.facebook.com/Wildan.ar.rahman">Wildan Ainurrahman</a> dengan lisensi <a href="https://github.com/w1lldone/bmtapp/blob/master/LICENSE">MIT</a>, dibuat dengan <a href="https://laravel.com/">Laravel</a> 5.4, <a href="http://getbootstrap.com/">Bootstrap</a> v4.0.0-beta
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
