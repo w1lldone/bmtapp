ec003faf02146c1606cafadb91b3088603151552
w1lldone <wildan.arrahman@gmail.com>
2017-09-06 16:25:02 +0700

    add template controller

---
+++ app/Http/Controllers/TemplateController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Template;
+use Illuminate\Http\Request;
+
+class TemplateController extends Controller
+{
+    function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Template  $template
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Template $template)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Template  $template
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Template $template)
+    {
+        return view('template.edit', compact('template'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Template  $template
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Template $template)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Template  $template
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Template $template)
+    {
+        //
+    }
+}
