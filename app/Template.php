b58ab675bfadc6c449dd100b8b611a38ae731ba5
w1lldone <wildan.arrahman@gmail.com>
2017-09-06 16:25:38 +0700

    add replace date code method;

--- app/Template.php
+++ app/Template.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Template extends Model
 {
+
+	public function replaceDate($string)
+	{
+		$this->foot = str_replace('{tanggal}', $string, $this->foot);
+
+		return $this; 
+	}
     
     protected $guarded = ['id'];
 }
