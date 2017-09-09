15450ef69dddbc693c016c104ef32ab5eefbbf79
w1lldone <wildan.arrahman@gmail.com>
2017-09-09 17:32:36 +0700

    add custom method for views

--- app/Feedback.php
+++ app/Feedback.php
@@ -14,6 +14,40 @@ class Feedback extends Model
 	public function feedback_kategori(){
 		return $this->belongsTo('App\FeedbackKategori');
 	}
+
+	/*CUSTOM METHOD*/
+	public function readStatus()
+	{
+		return empty($this->read_at) ? '' : 'text-muted';
+	}
+
+	public function kategoriColor()
+	{
+		switch ($this->feedback_kategori->name) {
+			case 'Bug':
+				$color = 'danger';
+				break;
+
+			case 'Pertanyaan':
+				$color = 'warning';
+				break;
+
+			case 'Masukan':
+				$color = 'info';
+				break;
+		}
+
+		if ($this->read_at != null) {
+			$color .= ' fade';
+		}
+
+		return $color;
+	}
+
+	public function isiReduced()
+	{
+		return substr($this->isi, 0, 50);
+	}
     
     protected $guarded = ['id'];
 }
