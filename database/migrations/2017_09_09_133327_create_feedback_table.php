4b4f75e673e7277c2c63a76a2f32764f9c985bca
w1lldone <wildan.arrahman@gmail.com>
2017-09-09 14:04:48 +0700

    add feedback models and tables

---
+++ database/migrations/2017_09_09_133327_create_feedback_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('nasabah_id')->unsigned();
+            $table->integer('feedback_kateogri_id')->unsigned();
+            $table->string('judul', 100)->nullable();
+            $table->string('isi', 500)->nullable();
+            $table->string('gambar', 250)->nullable();
+            $table->date('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+}
