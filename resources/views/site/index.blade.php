acb2b19e01d1f9558e4524f252033c2d99bc2942
w1lldone <wildan.arrahman@gmail.com>
2017-12-15 16:55:36 +0700

    add panduan on front page

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -8,11 +8,49 @@
 	<section class="py-5">
 		<div class="container container-fluid">
 			<div class="row">
-				<div class="col-md-4">
+				<div class="col-md-4 text-center">
 					<img src="/assets/img/background/shopping.svg" class="img-fluid mb-3">
 				</div>
 				<div class="col-md-8 d-flex align-items-center">
 					<img src="/assets/img/background/slogan.svg" class="img-fluid mb-3">
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="py-5">
+		<div class="container container-fluid">
+			<div class="row justify-content-md-center">
+				<div class="col-lg-4 col-md-6 mb-3">
+					<div class="card">
+						<div class="card-header text-center">
+							<h3 class="title">Panduan pembelian</h3>
+						</div>
+						<div class="card-body">
+							<div class="text-center">
+								<img src="/assets/img/icons/pembelian.svg" class="mb-3 img-fluid" width="200px"> <br>
+							</div>
+							Lihat betapa mudahnya transaksi dengan Bi-Mobile!
+						</div>
+						<div class="card-footer text-center">
+							<a href="/panduan/pembelian" class="btn btn-success">Baca panduan</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="card">
+						<div class="card-header text-center">
+							<h3 class="title">Panduan layanan</h3>
+						</div>
+						<div class="card-body">
+							<div class="text-center">
+								<img src="/assets/img/icons/layanan.svg" class="mb-3 img-fluid" width="200px"> <br>
+							</div>
+							Bi-Mobile menyediakan berbagai macam layanan pembayaran.
+						</div>
+						<div class="card-footer text-center">
+							<a href="/panduan/layanan" class="btn btn-success">Baca panduan</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
