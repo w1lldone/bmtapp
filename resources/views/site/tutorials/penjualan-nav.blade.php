645d68c0fbef177e979446f045a5dc448894fac7
w1lldone <wildan.arrahman@gmail.com>
2017-12-20 00:28:01 +0700

    fix penjualan nav

--- resources/views/site/tutorials/penjualan-nav.blade.php
+++ resources/views/site/tutorials/penjualan-nav.blade.php
@@ -3,7 +3,7 @@
     <div class="card tutorial-card">
       <nav class="nav nav-pills nav-fill">
         <a class="nav-item nav-link @if (Route::currentRouteName() == 'panduan.penjualan'){{ 'active' }} @endif" href="/panduan/penjualan">Jual produk</a>
-        <a class="nav-item nav-link @if (Route::currentRouteName() == 'panduan.produk.edit'){{ 'active' }} @endif" href="#">Ubah info produk</a>
+        <a class="nav-item nav-link @if (Route::currentRouteName() == 'panduan.edit.produk'){{ 'active' }} @endif" href="/panduan/edit-produk">Ubah info produk</a>
         <a class="nav-item nav-link @if (Route::currentRouteName() == 'panduan.transaksi'){{ 'active' }} @endif" href="/panduan/transaksi">Kelola transaksi</a>
       </nav>
     </div>
